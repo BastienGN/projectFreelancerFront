a0c236debdd039ac46546f2189eb83c7b8847a0b
Zzaurel <aurelienflechelle@orange.fr>
2022-01-21 16:23:26 +0100

    Aurel Methode spécific GET Util+Test

--- src/app/services/utilisateur.service.ts
+++ src/app/services/utilisateur.service.ts
@@ -2,12 +2,22 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpRequest } from '@angular/common/http';
 import { Utilisateur } from '../models/utilisateur';
+import { Role } from '../models/role';
+import { Candidature } from '../models/candidature';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtilisateurService {
   private baseURL= "http://localhost:8080/utilisateurs";
+  private baseURL2= "http://localhost:8080/util";
+  private baseURL3= "http://localhost:8080/utilisateurscandidature";
+  private baseURL4= "http://localhost:8080/freelancersprojets";
+  private baseURL5= "http://localhost:8080/moyennefreelancer";
+  private baseURL6= "http://localhost:8080/moyennejobowner";
+  private baseURL7= "http://localhost:8080/nombrefreelancer";
+  private baseURL8= "http://localhost:8080/nombrejobowner";
+
   constructor(private httpClient: HttpClient) { }
 
   public findAll(): Observable<any> {
@@ -31,4 +41,33 @@ export class UtilisateurService {
     const req = new HttpRequest('POST',this.baseURL,formData, {reportProgress: true, responseType: 'text'});
     return this.httpClient.request(req);
     } 
+
+    public findByRole(libelle_role: string): Observable<any>{
+      return this.httpClient.get(this.baseURL2 + "/" + libelle_role);
+    }
+
+    public findUtilisateurByCandidature(idU: number): Observable<any>{
+      return this.httpClient.get(this.baseURL3+ "/" + idU);
+    }
+
+    public findFreelancerByProject(idF: number): Observable<any>{
+      return this.httpClient.get(this.baseURL4 + "/" + idF);
+    }
+
+    public findMoyenneByFreeLancer(idF: number): Observable<any>{
+      return this.httpClient.get(this.baseURL5 + "/" + idF);
+    }
+
+    public findMoyenneByJobOwner(idJ: number): Observable<any>{
+      return this.httpClient.get(this.baseURL6 + "/" + idJ);
+    }
+
+    public nombreFreelancer(): Observable<any>{
+      return this.httpClient.get(this.baseURL7);
+    }
+
+    public nombreJobOwner(): Observable<any>{
+      return this.httpClient.get(this.baseURL8);
+    }
+
 }
